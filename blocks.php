d55516893d3ae997e2f2249c8cbf8e0d7e0b1fa8
Alpagga <lukas.schmid.01@icloud.com>
2026-03-04 18:53:43 +0100

    Rechtschreibfehler

--- blocks.php
+++ blocks.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Block registrieren
 function cbn_register_navbar_block() {
@@ -24,31 +23,4 @@ function cbn_render_navbar() {
     ob_start();
     include get_template_directory() . '/block/render.php';
     return ob_get_clean();
-=======
-<?php
-// Block registrieren
-function cbn_register_navbar_block() {
-    // JS für den Editor
-    wp_register_script(
-        'cbn-navbar-block',
-        get_template_directory_uri() . '/block/index.js',
-        array('wp-blocks','wp-element','wp-editor'),
-        filemtime(get_template_directory() . '/block/index.js'),
-        true
-    );
-
-    // Block registrieren mit PHP Render Callback
-    register_block_type('cbn/navbar', array(
-        'editor_script' => 'cbn-navbar-block',
-        'render_callback' => 'cbn_render_navbar'
-    ));
-}
-add_action('init', 'cbn_register_navbar_block');
-
-// Render Callback
-function cbn_render_navbar() {
-    ob_start();
-    include get_template_directory() . '/block/render.php';
-    return ob_get_clean();
->>>>>>> c28808a4cf6732635907c4c3c8b6c85a7d87d235
 }
--- page-scrollblocks.php
+++ page-scrollblocks.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Template Name: Scroll Blocks
@@ -6,12 +5,3 @@
  */
 get_header();
 ?>
-=======
-<?php
-/**
- * Template Name: Scroll Blocks
- * Description: Seite mit vertikal/horizontal scrollenden Blöcken
- */
-get_header();
-?>
->>>>>>> c28808a4cf6732635907c4c3c8b6c85a7d87d235
